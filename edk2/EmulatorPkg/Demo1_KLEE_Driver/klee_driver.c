4ed4e1345105387e17eb4afd46a56f376fe7ea8a
shafiuzzaman-md <md.shafiuzzaman.hira@gmail.com>
2023-05-30 15:29:07 -0700

    Given knowledge on READ_MAGIC (upper 64bit), attacker can identify KEY_MAGIC (lower 64bit) using Demo1BobDataProvider

--- edk2/EmulatorPkg/Demo1_KLEE_Driver/klee_driver.c
+++ edk2/EmulatorPkg/Demo1_KLEE_Driver/klee_driver.c
@@ -4,38 +4,36 @@
 #include <stdlib.h>
 
 #define MEMORY_SIZE 1000
-// BobKey
-DEMO1_ACCESS_KEY   *AccessKeyPtr;
+
 
 void ScanMemoryForKey(UINTN read_magic) {
-  char *addr;
-  DEMO1_ACCESS_KEY *data;
-  size_t size;
-  
-  // Allocate memory
-  size = sizeof(read_magic);
-  data = (DEMO1_ACCESS_KEY*) malloc(sizeof(DEMO1_ACCESS_KEY));
-  addr = (char*) malloc(MEMORY_SIZE);
-
+    DEMO1_ACCESS_KEY *addr;
+    VOID *dataptr;
+    size_t size;
     
-  // Set symbolic values and constraints
-  klee_make_symbolic(addr, MEMORY_SIZE, "addr");
-  klee_assume(data->access_key_store[1] != 0);
-  
-  //Run symbolic execution
-  if(Demo1BobDataProvider(NULL, addr, (VOID**)&data, size) == EFI_SUCCESS)
-  {
-    if (data->access_key_store[0] == read_magic) {
-          printf("read_magic: %llx\n", read_magic);
-          printf("data->access_key_store[0]: %llx\n", data->access_key_store[0]);
-          printf("data->access_key_store[1]: %llx\n", data->access_key_store[1]);
-          klee_assert(0 && "bobKey identified");
+    // Allocate memory for data(Dest)
+    dataptr = (char*) malloc(size);;
+    addr = (DEMO1_ACCESS_KEY*) malloc(MEMORY_SIZE);
+
+    // Set symbolic values and constraints for addr and size
+    klee_make_symbolic(addr, MEMORY_SIZE, "addr");
+    klee_make_symbolic(&size, sizeof(UINTN), "size");
+    klee_assume(addr->access_key_store[1] == read_magic);
+
+    // Run symbolic execution
+    if (Demo1BobDataProvider(NULL, addr, &dataptr, size) == EFI_SUCCESS)
+    {
+         klee_assert(memcmp((VOID *)dataptr, (VOID *)&bobKey, sizeof(bobKey)) != 0);
+         printf("addr->access_key_store[0]: %x\n", addr->access_key_store[0]);
+         printf("addr->access_key_store[1]: %llx\n", addr->access_key_store[1]);
+
     }
-  }
-  // Free the dynamically allocated memory
-  free(addr);
-  free(data);
+    
+    // Free the dynamically allocated memory
+    free(addr);
+    free(dataptr);
 }
+
 
 
 void scan_memory(){
@@ -45,10 +43,16 @@ void scan_memory(){
   // Make gLoadImage symbolic
   klee_make_symbolic(gLoadImage, sizeof(EFI_LOADED_IMAGE_PROTOCOL), "gLoadImage");
 
+  // Generate BobKey with read access
+  DEMO1_ACCESS_KEY   *AccessKeyPtr;
   AccessKeyPtr = &bobKey;
   AccessKeyPtr->access_key_store[0]=rand();
-  UINTN read_magic = (ACCESS_KEY_MAGIC << MAGIC_SIZE) + READ_ACCESS;
+  UINTN read_magic;
+  read_magic = (ACCESS_KEY_MAGIC << MAGIC_SIZE) + READ_ACCESS;
   AccessKeyPtr->access_key_store[1] = read_magic;
+
+  printf("KEY_MAGIC: %x\n", (UINT8 *)AccessKeyPtr->access_key_store[0]);
+  printf("READ_MAGIC: %llx\n", AccessKeyPtr->access_key_store[1]);
 
   ScanMemoryForKey(read_magic);
 }
